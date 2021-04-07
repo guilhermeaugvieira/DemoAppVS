63817c4812eb988cda56bb2878ac3567084237e4
Guilheme Augusto Vieira <guilhermeavdino@gmail.com>
2021-04-07 14:36:19 -0300

    .Net Core Basic
    Introdução ao .Net Core

---
+++ Middleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace DemoAppVS
+{
+    public class Middleware
+    {
+        public readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("\n\r ------ Antes ------ \n\r");
+
+            await _next(context);
+
+            Console.WriteLine("\n\r ------ Depois ------ \n\r");
+        }
+    }
+
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseMiddlewareExtension(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();                   
+        }
+    }
+}
